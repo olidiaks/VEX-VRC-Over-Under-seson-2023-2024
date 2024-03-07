9d6d3525943c971fbda8795bdf2e55f2d472085d
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-03-07 08:25:06 -0600

    Change time in autonomus again.

--- src/autonomus.cpp
+++ src/autonomus.cpp
@@ -30,7 +30,7 @@ extern bool isRightOpen;
 
 void Autonomus::writeAutonomusStream() {
   counter++;
-  if (counter > 3) {
+  if (counter > 6) {
     std::ofstream ofs("/usd/autonomusStream.hpp", std::ios::app);
     ofs << leftFrontDiveTrainMotor.get_voltage() << ",\t"
         << rightFrontDiveTrainMotor.get_voltage() << ",\t"

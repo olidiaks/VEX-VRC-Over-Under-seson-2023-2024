3bafeda13c52719b7e1d603abfe72195dcf32ca7
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-03-07 08:08:08 -0600

    Last atempt ot fix printing status on controller.

--- src/printStatusOnController.cpp
+++ src/printStatusOnController.cpp
@@ -1,19 +1,25 @@
 #include "main.h"
 #include <cstdio>
+#include <sstream>
+#include <string>
 
 int printStatusOnControllerIterator = 0;
 
 void printStatusOnController() {
   printf("%d\n", printStatusOnControllerIterator);
-  printStatusOnControllerIterator ++;
-  if (printStatusOnControllerIterator > 10000) {
+  printStatusOnControllerIterator++;
+  if (printStatusOnControllerIterator > 100) {
+    std::string output = "";
+    std::stringstream ss;
+    ss << speedPrecent << "|" << leftFrontDiveTrainMotor.get_temperature()
+       << leftBackDiveTrainMotor.get_temperature()
+       << rightFrontDiveTrainMotor.get_temperature()
+       << rightBackDiveTrainMotor.get_temperature()
+       << launcherAMotor.get_temperature() << "|"
+       << launcherBMotor.get_temperature();
+
+    ss >> output;
+    controller.set_text(3, 1, output);
     printStatusOnControllerIterator = 0;
-    controller.print(3, 0, "%d | %d %d %d %d | %d %d", speedPrecent,
-                     leftFrontDiveTrainMotor.get_temperature(),
-                     leftBackDiveTrainMotor.get_temperature(),
-                     rightFrontDiveTrainMotor.get_temperature(),
-                     rightBackDiveTrainMotor.get_temperature(),
-                     launcherAMotor.get_temperature(),
-                     launcherBMotor.get_temperature());
   }
 }

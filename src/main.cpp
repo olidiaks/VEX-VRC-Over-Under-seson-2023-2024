7e5e1bfa5a9f7918490b0f6f07daa653223ab124
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-04-02 09:27:45 -0500

    Delete unnnecsesary delay.

--- src/main.cpp
+++ src/main.cpp
@@ -68,8 +68,6 @@ void opcontrol() {
   Autonomus selfDriving;
   controller.rumble("..");
 
-  int i = 0;
-
   while (true) {
     changeSpeedOfDriveTraind(&speedPrecent);
     setMotorsToDriveFromControler(100);
@@ -82,18 +80,12 @@ void opcontrol() {
     airSystemSetAirSystemBasedOnControllerAction();
     selfDriving.writeAutonomusStream();
 
-    if (i > 200) {
-
-      pros::lcd::print(2, "Launcher %.0f %.0f",
-                       launcherAMotor.get_temperature(),
-                       launcherBMotor.get_temperature());
-      pros::lcd::print(3, "Drive train: lf %.0f lb %.0f rf %.0f rb %.0f", 
-          leftFrontDiveTrainMotor.get_temperature(),
-          leftBackDiveTrainMotor.get_temperature(),
-          rightFrontDiveTrainMotor.get_temperature(),
-          rightBackDiveTrainMotor.get_temperature());
-      i = 0;
-    }
-    ++i;
+    pros::lcd::print(2, "Launcher %.0f %.0f", launcherAMotor.get_temperature(),
+                     launcherBMotor.get_temperature());
+    pros::lcd::print(3, "Drive train: lf %.0f lb %.0f rf %.0f rb %.0f",
+                     leftFrontDiveTrainMotor.get_temperature(),
+                     leftBackDiveTrainMotor.get_temperature(),
+                     rightFrontDiveTrainMotor.get_temperature(),
+                     rightBackDiveTrainMotor.get_temperature());
   }
 }

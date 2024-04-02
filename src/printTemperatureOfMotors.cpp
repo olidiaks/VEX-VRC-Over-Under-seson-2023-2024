53060d0a2b369e405d31c47bad0e5bad114b0ee1
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-04-02 11:36:14 -0500

    Add difrentaion wchich motor is which in launcher.

--- src/printTemperatureOfMotors.cpp
+++ src/printTemperatureOfMotors.cpp
@@ -6,8 +6,8 @@ void printTemperatureOfMotrs() {
   pros::lcd::print(2, "Graber l %.0f r %.0f",
                    leftTriaballGraberMotor.get_temperature(),
                    rightTriaballGraberMotor.get_temperature());
-  pros::lcd::print(3, "Launcher %.0f %.0f", launcherAMotor.get_temperature(),
-                   launcherBMotor.get_temperature());
+  pros::lcd::print(3, "Launcher f %.0f b %.0f", launcherBMotor.get_temperature(),
+                   launcherAMotor.get_temperature());
   pros::lcd::print(4, "Drive train: lf %.0f lb %.0f rf %.0f rb %.0f",
                    leftFrontDiveTrainMotor.get_temperature(),
                    leftBackDiveTrainMotor.get_temperature(),
